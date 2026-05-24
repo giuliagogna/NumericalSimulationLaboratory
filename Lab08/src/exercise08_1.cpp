6ddb3ea72ab9b145013970a25bf9e7f1ff8cacb4
Giulia Gogna <giulia.gogna@gmail.com>
2026-05-24 11:59:48 +0200

    Add possibility for dropout (bad)

---
+++ Lab08/src/exercise08_1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello 08.1" << endl;
+}
---
+++ Lab08/src/exercise08_2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello 08.2" << endl;
+}
